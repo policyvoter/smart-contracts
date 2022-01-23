ccbea4f53ca4e65f1310a28eb663fece04669495
x <x@to.wtf>
2022-01-23 19:11:43 +0200

    :fire:

--- gen_bindings.sh
+++ gen_bindings.sh
@@ -3,3 +3,4 @@ npx hardhat export-abi
 abigen --abi abi/PolicyVoterV0.json --pkg pv0_smartcontract --out bindings.go
 rm -rf abi/
 echo "generated bindings.go"
+echo "remember to change the package name to `eth` when you copy paste"
